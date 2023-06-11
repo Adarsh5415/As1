606ee965fea6b7162db6a394813804edb06d51e4
Adarsh5415 <anurag1032000@gmail.com>
2023-06-11 12:54:13 +0530

    all cases passed

--- q2.js
+++ q2.js
@@ -1,6 +1,5 @@
 var i=1;
 while(i<=15){
-
     if(i==1)
     {
 
@@ -8,7 +7,6 @@ while(i<=15){
     else if(i==2 || i==3){
         console.log(i);
     }
-
     else if(i%2!=0 && i%3!=0)
     {
     console.log(i)
