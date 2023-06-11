a2b499ae4046b213782db66c54aaa148a898c7f1
Adarsh5415 <136148151+Adarsh5415@users.noreply.github.com>
2023-06-11 12:56:53 +0530

    Update q2.js

--- q2.js
+++ q2.js
@@ -1,6 +1,13 @@
 var i=1;
-while(i<10){
-    if(i%2!=0)
+while(i<=15){
+    if(i==1)
+    {
+
+    }
+    else if(i==2 || i==3){
+        console.log(i);
+    }
+    else if(i%2!=0 && i%3!=0)
     {
     console.log(i)
     }
