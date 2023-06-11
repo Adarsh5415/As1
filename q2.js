e360431a619b427f7d19657e893eada8aeda2253
Adarsh5415 <anurag1032000@gmail.com>
2023-06-11 12:28:30 +0530

    all test case passed

--- q2.js
+++ q2.js
@@ -1,8 +1,18 @@
 var i=1;
-while(i<10){
-    if(i%2!=0)
+while(i<=15){
+
+    if(i==1)
+    {
+
+    }
+    else if(i==2 || i==3){
+        console.log(i);
+    }
+
+    else if(i%2!=0 && i%3!=0)
     {
     console.log(i)
     }
     i++;
 }
+
