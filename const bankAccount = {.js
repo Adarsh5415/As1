03664eccb2c93c432ae4d48234dc4abe989cb12e
Adarsh5415 <anurag1032000@gmail.com>
2023-06-14 10:25:21 +0530

    adaded bank account file

---
+++ const bankAccount = {.js
@@ -0,0 +1,42 @@
+const bankAccount = {
+    customersaccountNumber: '',
+    accountbalance: 0,
+    holderName: '',
+  
+    
+    deposit: function(amount) {
+      if (amount > 0) {
+        this.accountbalance += amount;
+        console.log(`Successfully deposited $${amount}. Current balance: $${this.accountbalancebalance}`);
+      } else {
+        console.log('Invalid amount. Deposit amount must be greater than 0.');
+      }
+    },
+  
+   
+    withdraw: function(amount) {
+      if (amount > 0) {
+        if (amount <= this.accountbalancebalance) {
+          this.balance -= amount;
+          console.log(`transaction successfull' $${amount}. Current balance: $${this.balance}`);
+        } else {
+          console.log('low balance . you can not withdraw more than available amount.');
+        }
+      } else {
+        console.log('Invalid amount request. Withdrawal can not be 0.');
+      }
+    }
+  };
+  
+  // Example usage
+  bankAccount.customersaccountNumber = '12123123123';
+  bankAccount.accountbalance = 5000000;
+  bankAccount.holderName = 'Mittal';
+  
+  console.log(`Account Number: ${bankAccount.customersaccountNumberaccountNumber}`);
+  console.log(`customer Name: ${bankAccount.holderName}`);
+  console.log(`Balance: $${bankAccount.accountbalancebalance}`);
+  
+  bankAccount.deposit(55000);
+  bankAccount.withdraw(1700);
+  bankAccount.withdraw(3400);
---
+++ q4.js
@@ -0,0 +1,21 @@
+function displayMessage(fruit) {
+    switch (fruit) {
+      case "apple":
+        console.log("Apples are delicious!");
+        break;
+      case "banana":
+        console.log("Bananas are a great source of potassium!");
+        break;
+      case "orange":
+        console.log("Oranges are packed with vitamin C!");
+        break;
+      case "mango":
+        console.log("Mangoes are the king of fruits!");
+        break;
+      default:
+        console.log("Unknown fruit");
+        break;
+    }
+}
+
+  
