1600863a18f30572446c7c5945ad1a17400dd555
Adarsh5415 <anurag1032000@gmail.com>
2023-06-14 19:31:56 +0530

    added new file

--- const bankAccount = {.js
+++ const bankAccount = {.js
@@ -7,7 +7,7 @@ const bankAccount = {
     deposit: function(amount) {
       if (amount > 0) {
         this.accountbalance += amount;
-        console.log(`Successfully deposited $${amount}. Current balance: $${this.accountbalancebalance}`);
+        console.log(`Successfully deposited {amount}. Current balance: {this.accountbalancebalance}`);
       } else {
         console.log('Invalid amount. Deposit amount must be greater than 0.');
       }
@@ -18,7 +18,7 @@ const bankAccount = {
       if (amount > 0) {
         if (amount <= this.accountbalancebalance) {
           this.balance -= amount;
-          console.log(`transaction successfull' $${amount}. Current balance: $${this.balance}`);
+          console.log(`transaction successfull' {amount}. Current balance: {this.balance}`);
         } else {
           console.log('low balance . you can not withdraw more than available amount.');
         }
@@ -33,9 +33,9 @@ const bankAccount = {
   bankAccount.accountbalance = 5000000;
   bankAccount.holderName = 'Mittal';
   
-  console.log(`Account Number: ${bankAccount.customersaccountNumberaccountNumber}`);
-  console.log(`customer Name: ${bankAccount.holderName}`);
-  console.log(`Balance: $${bankAccount.accountbalancebalance}`);
+  console.log(`Account Number: {bankAccount.customersaccountNumberaccountNumber}`);
+  console.log(`customer Name: {bankAccount.holderName}`);
+  console.log(`Balance: {bankAccount.accountbalancebalance}`);
   
   bankAccount.deposit(55000);
   bankAccount.withdraw(1700);
